675edc4748f5a729017da0c2886ebf45e779c7d5
Prashant Pathak <144862456+Prashant7pathak@users.noreply.github.com>
2024-10-14 14:31:35 +0530

    Add files via upload

---
+++ js1-01.js
@@ -0,0 +1,7 @@
+const massMark = 78;
+const massJohn = 92;
+const heightMark = 1.69;
+const heightJohn = 1.95;
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark,BMIJohn);
